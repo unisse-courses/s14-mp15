1d31ffee223798dc04fe6efa8e692765b88b9767
nikeplanas <36704277+nikeplanas@users.noreply.github.com>
2020-11-14 13:42:15 +0800

    removed post call (not needed here)

--- mp2/public/js/reserves.js
+++ mp2/public/js/reserves.js
@@ -76,42 +76,5 @@ $(document).ready(function() {
     document.getElementById('date').setAttribute("min", year + "-" + month[d.getMonth()]+ "-01");
     document.getElementById('date').setAttribute("max", year + "-" + month[d.getMonth()]+ "-30");
     /*Change max day to 30/31 depending on month */
-    
-
-     // POST called
-     $('#addReservation').click(function() {
-        // Get the data from the form
-        var pax = parseInt($('#pax').val());
-
-        var date = ($('#date').val());
-
-        var timeslot = $('#timeslot').val();
-
-        console.log(pax, date, timeslot);
-
-      var reservation = 
-      {
-      pax: pax,
-      date: date,
-      timeslot: timeslot, 
-  
-      };
-
-        $.post('addReservation', newReservation, function(data, status) {
-           console.log(data);
-      
-          if (data.success) {
-            $('#msg').text(data.message);
-            $('#msg').addClass('success');
-      
-            $('#pax').val('');
-            $('#date').val('');
-            $('#timeslot').val('');
-          } else {
-            $('#msg').text(data.message);
-            $('#msg').addClass('fail');
-          }
-        });        
-      });
 
 });
